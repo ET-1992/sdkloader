cf445f7c0ee6b0882aa495bbaeb9ff03c1816460
giving <jirong.gjr@alibaba-inc.com>
2017-08-25 10:54:39 +0800

    feat: UC评论模块

--- src/sdkload_comment_detail.js
+++ src/sdkload_comment_detail.js
@@ -1,7 +1,7 @@
 import './loader';
 
 if (window.location.host.indexOf('release') > -1) {
-  window.sdkLoader({mapPath: '//image.uc.cn/s/uae/g/1y/ucCommentSdk/releaseCommentDetailSdkJson.json'});
+  window.sdkLoader({mapPath: '//image.uc.cn/s/uae/g/1y/ucCommentSdk/release/releaseCommentDetailSdkJson.json'});
 } else {
-  window.sdkLoader({mapPath: '//image.uc.cn/s/uae/g/1y/ucCommentSdk/commentDetailSdkJson.json'});
+  window.sdkLoader({mapPath: '//image.uc.cn/s/uae/g/1y/ucCommentSdk/prod/commentDetailSdkJson.json'});
 }
